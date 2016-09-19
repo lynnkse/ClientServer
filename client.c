a7f28ffa2d4427ce9455e60ffc94ec2c39bc4284
Anton Gulyaev <lynnkse@gmail.com>
2016-09-19 17:55:22 +0300

    working version

--- client.c
+++ client.c
@@ -5,17 +5,22 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int main()
 {
 	int soc;
 	struct sockaddr_in sockAddr;
-	unsigned char bytes[] = 
+	
+	unsigned char bytes[] = "hahahahaha";
+	
+	/*unsigned char bytes[] = 
 	{
 		0x5a ,0x76 ,0x01 ,0x00 ,0x00 ,0x01 ,0x00 ,0x00 ,0x00 ,0x00 ,0x00 ,0x00 ,0x03 ,0x77 ,0x77 ,0x77,
 		0x03 ,0x67 ,0x74 ,0x61 ,0x04 ,0x75 ,0x66 ,0x72 ,0x6a ,0x02 ,0x62 ,0x72 ,0x00 ,0x00 ,0x01 ,0x00,
 		0x01
-	};
+	};*/
 	
 	if((soc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
 	{
@@ -25,10 +30,10 @@ int main()
 	memset(&sockAddr, 0, sizeof(struct sockaddr));
 
 	sockAddr.sin_family = AF_INET;
-    sockAddr.sin_port = htons(53);
-    inet_aton("80.179.52.100", &sockAddr.sin_addr);
+    sockAddr.sin_port = htons(1025);
+    inet_aton(/*"80.179.52.100"*/"192.168.0.46", &sockAddr.sin_addr);
     	
-	sendto(soc, bytes, 34, 0, (const struct sockaddr *) &sockAddr, sizeof(struct sockaddr_in));
+	sendto(soc, bytes, 10, 0, (const struct sockaddr *) &sockAddr, sizeof(struct sockaddr_in));
 	
 	close(soc);
 	
--- server.c
+++ server.c
@@ -1,3 +1,47 @@
+/*   #include <arpa/inet.h>
+       #include <netinet/in.h>
+       #include <stdio.h>
+       #include <sys/types.h>
+       #include <sys/socket.h>
+       #include <unistd.h>
+     
+       #define BUFLEN 512
+       #define NPACK 10
+      #define PORT 1025
+    
+      void diep(char *s)
+      {
+        perror(s);
+        exit(1);
+      }
+    
+      int main(void)
+      {
+        struct sockaddr_in si_me, si_other;
+        int s, i, slen=sizeof(si_other);
+        char buf[BUFLEN];
+    
+        if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1)
+          diep("socket");
+    
+        memset((char *) &si_me, 0, sizeof(si_me));
+        si_me.sin_family = AF_INET;
+        si_me.sin_port = htons(PORT);
+        si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+        if (bind(s, &si_me, sizeof(si_me))==-1)
+            diep("bind");
+    
+        for (i=0; i<NPACK; i++) {
+          if (recvfrom(s, buf, BUFLEN, 0, &si_other, &slen)==-1)
+         diep("recvfrom()");
+      printf("Received packet from %s:%d\nData: %s\n\n", 
+            inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port), buf);
+   }
+
+close(s);
+return 0;
+}*/
+
 #include <sys/types.h>         
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -5,6 +49,8 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int main()
 {
@@ -22,16 +68,18 @@ int main()
 	memset(&sockAddr, 0, sizeof(struct sockaddr));
 
 	sockAddr.sin_family = AF_INET;
-    sockAddr.sin_port = htons(53);
-    sockAddr.s_addr = htonl(INADDR_ANY);
+    sockAddr.sin_port = htons(1025);
+    sockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
     
-    bind(soc, &sockAddr, sizeof(sockAddr));
+    bind(soc, (const struct sockaddr *) &sockAddr, sizeof(sockAddr));
     
-    recvfrom(soc, buf, 10, 0, &s_other, &len);
+    recvfrom(soc, buf, 10, 0, (struct sockaddr *) &s_other, (socklen_t *) &len);
     
-    printf("Recieved: %s\n", buf);
+    printf("Server recieved: %s\n", buf);
     
     close(soc);
+    
+    return 0;
 }
 
 
