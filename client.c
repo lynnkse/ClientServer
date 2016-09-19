e9deff9ef05a892e34fcb47ac59d7cad437725da
Anton Gulyaev <lynnkse@gmail.com>
2016-09-19 18:03:10 +0300

    both client and server send and recieve useless code deleted

--- client.c
+++ client.c
@@ -16,13 +16,6 @@ int main()
 	unsigned char bytes[] = "hahahahaha";
 	char buf[10];
 	
-	/*unsigned char bytes[] = 
-	{
-		0x5a ,0x76 ,0x01 ,0x00 ,0x00 ,0x01 ,0x00 ,0x00 ,0x00 ,0x00 ,0x00 ,0x00 ,0x03 ,0x77 ,0x77 ,0x77,
-		0x03 ,0x67 ,0x74 ,0x61 ,0x04 ,0x75 ,0x66 ,0x72 ,0x6a ,0x02 ,0x62 ,0x72 ,0x00 ,0x00 ,0x01 ,0x00,
-		0x01
-	};*/
-	
 	if((soc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
 	{
 		printf("Socket failed\n");
@@ -34,7 +27,7 @@ int main()
 
 	sockAddr.sin_family = AF_INET;
     sockAddr.sin_port = htons(1025);
-    inet_aton(/*"80.179.52.100"*/"192.168.0.46", &sockAddr.sin_addr);
+    inet_aton("192.168.0.46", &sockAddr.sin_addr);
     	
 	sendto(soc, bytes, 10, 0, (const struct sockaddr *) &sockAddr, sizeof(struct sockaddr_in));
 	
